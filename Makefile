88a75ddb1840afefc9a7500d91788197c4aae3fc
Rob Currie <rcurrie@ucsc.edu>
2016-11-25 18:52:58 -0800

    Defuse sort of working along with copying of files, still messy and no consistency between azure and openstack

--- Makefile
+++ Makefile
@@ -1,29 +1,34 @@
-ts := `/bin/date "+%Y%m%d-%H%M%S"`
+ts := $(shell /bin/date "+%Y%m%d-%H%M%S")
+
+AZURE_FLAVOR=Standard_D1
+OS_FLAVOR=m1.small
 
 create-openstack:
+	# Start an openstack docker-machine, specify size by make create-openstack OS_FLAVOR=m1.small
 	docker-machine create --driver openstack \
 		--openstack-tenant-name treehouse \
 		--openstack-auth-url http://os-con-01.pod:5000/v2.0 \
-		--openstack-flavor-name m1.small \
 		--openstack-ssh-user ubuntu \
 		--openstack-net-name treehouse-net \
 		--openstack-floatingip-pool ext-net \
 		--openstack-image-name Ubuntu-16.04-LTS-x86_64 \
+		--openstack-flavor-name $(OS_FLAVOR) \
 		$(USER)-treeshop-$(ts)
 	docker-machine ssh $(USER)-treeshop-$(ts) sudo gpasswd -a ubuntu docker
 
 create-azure:
+	# Start an azure docker-machine, specify size by make create-azure AZURE_FLAVOR=Standard_D1
 	# Sizes: https://docs.microsoft.com/en-us/azure/virtual-machines/virtual-machines-linux-sizes
 	docker-machine create --driver azure \
 		--azure-subscription-id 11ef7f2c-6e06-44dc-a389-1d6b1bea9489 \
 		--azure-resource-group treeshop \
-		--azure-username ubuntu \
+		--azure-ssh-user ubuntu \
 		--azure-image canonical:UbuntuServer:16.04.0-LTS:latest \
-		--azure-size Standard_D1 \
+		--azure-size $(AZURE_FLAVOR) \
 		$(USER)-treeshop-$(ts)
 	docker-machine ssh $(USER)-treeshop-$(ts) sudo gpasswd -a ubuntu docker
 
 terminate:
-	-docker-machine stop $(USER)-treeshop-0
-	-docker-machine rm -f $(USER)-treeshop-0
-
+	# Stop and remove ALL docker-machines in ALL environments
+	-docker-machine stop `docker-machine ls --format '{{.Name}}'`
+	-docker-machine rm -f `docker-machine ls --format '{{.Name}}'`
--- fabfile.py
+++ fabfile.py
@@ -14,8 +14,10 @@ line to log in via URL
 """
 import os
 import re
+import datetime
 import csv
-from fabric.api import env, local, run, runs_once
+from fabric.api import env, local, run, runs_once, parallel
+from fabric.contrib.files import exists
 
 """
 Setup the fabric hosts environment using docker-machine
@@ -40,14 +42,11 @@ def machines():
     print "SSH Keys:", env.key_filename
 
 
-def hello():
-    """ Run echo $HOSTNAME on each machine inside a docker container """
-    run("docker run alpine /bin/echo ""Hello from $HOSTNAME""")
-
-
 def create(count=1, flavor="m1.small"):
     """ Create 'count' (default=1) 'flavor' (default=m1.small) machines """
     for i in range(0, int(count)):
+        name = "{}-treeshop-{}".format(os.environ["USER"],
+                                       datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
         local("""
             docker-machine create --driver openstack \
                 --openstack-tenant-name treehouse \
@@ -57,11 +56,10 @@ def create(count=1, flavor="m1.small"):
                 --openstack-net-name treehouse-net \
                 --openstack-floatingip-pool ext-net \
                 --openstack-image-name Ubuntu-16.04-LTS-x86_64 \
-                {}-treeshop-{}
-              """.format(flavor, os.environ["USER"], i))
+                {}
+              """.format(flavor, name))
         # Add ubuntu to docker group so we can do run("docker...") vs. sudo
-        local("docker-machine ssh {}-treeshop-{} sudo gpasswd -a ubuntu docker"
-              .format(os.environ["USER"], i))
+        local("docker-machine ssh {} sudo gpasswd -a ubuntu docker".format(name))
 
 
 def terminate():
@@ -71,10 +69,66 @@ def terminate():
         local("docker-machine rm -f {}".format(host))
 
 
-@runs_once
+@parallel
+def hello():
+    """ Run echo $HOSTNAME on each machine inside a docker container """
+    print "Running against", env.host
+    run("docker run alpine /bin/echo ""Hello from $HOSTNAME""")
+
+
+@parallel
+def configure():
+    """ Configure folders on each machine """
+    run("sudo mkdir -p /mnt/inputs")
+    run("sudo chown ubuntu:ubuntu /mnt/inputs")
+    run("sudo mkdir -p /mnt/outputs")
+    run("sudo chown ubuntu:ubuntu /mnt/outputs")
+    run("sudo mkdir -p /mnt/references")
+    run("sudo chown ubuntu:ubuntu /mnt/references")
+
+
+@parallel
+def defuse_references():
+    # run("sudo wget -P /mnt/references http://ceph-gw-01.pod/references/defuse_index.tar.gz")
+    local("docker-machine scp"
+          "/pod/pstore/users/jpfeil/references/defuse_index.tar.gz {}:/mnt/references"
+          .format(env.host))
+
+
 def defuse(manifest):
-    """ Run defuse on all fastq pairs in 'manifest' """
+    """ Run defuse on all the samples in 'manifest' """
     with open(manifest) as f:
         for row in csv.DictReader(f, delimiter="\t"):
-            for file in row["samples"].split(","):
-                local("scp {} {}".format(file, env.hosts[0]))
+            for sample in row["File Path"].split(","):
+                dest = "/mnt/inputs/{}".format(os.path.basename(sample))
+                if exists(dest):
+                    print "Skipping, already exists".format(dest)
+                else:
+                    print "Copying {} to {}".format(sample, dest)
+                    local("docker-machine scp {} {}:{}".format(sample, env.hostnames[0], dest))
+            run("""
+                docker run -it --rm \
+                    -v /mnt/inputs:/data \
+                    -v /mnt/outputs:/outputs \
+                    -v /mnt/references/defuse_index:/data/defuse_index \
+                    jpfeil/defuse-pipeline:latest \
+                    -1 {} -2 {} \
+                    -o /outputs \
+                    -n {}
+                """.format(os.path.basename(row["File Path"].split(",")[0]),
+                           os.path.basename(row["File Path"].split(",")[1]),
+                           row["Submitter Sample ID"]))
+            return
+
+
+def defuse_one():
+    run("""
+        docker run -it --rm \
+            -v /mnt/inputs:/data \
+            -v /mnt/outputs:/outputs \
+            -v /mnt/references/defuse_index:/data/defuse_index \
+            jpfeil/defuse-pipeline:latest \
+            -1 SRR1988322_1.fastq.gz -2 SRR1988322_2.fastq.gz \
+            -o /outputs \
+            -n SRR1988322
+        """)
